70e8b58ad6c601febad02d923d54911634296b2e
dedencLichKing <165057345@qq.com>
2023-12-13 23:52:17 +0800

    xcode
    xcode init

---
+++ NativeProject/NativeProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NativeProject
+//
+//  Created by dedenc on 2023/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func pushFlutter(_ sender: UIButton) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
